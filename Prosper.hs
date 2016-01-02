7ebd65db54dfa5aed2c4005f17f01a19fbd63ab3
Friede <friede80@gmail.com>
2016-01-02 01:02:42 -0500

    Refactored to extract general function for retrieving paginated responses

--- Prosper.hs
+++ Prosper.hs
@@ -8,10 +8,9 @@ import Data.Text.Strict.Lens
 import Network.Wreq
 import Data.Text (Text)
 import Data.List
+import Data.Maybe
 import System.Environment
-
 import qualified Data.Vector as V
-import Debug.Trace
 
 import Notes
 
@@ -38,66 +37,61 @@ oauthToken clientID clientSecret userID password = do
     resp <- postWith opts (prosperURL "security/oauth/token") body
     return (resp ^. responseBody . key "access_token" . _String)
 
--- | Performs a GET request to the provided target using the provided
--- OAuth token.
+-- | Performs a GET request to the provided target using the provided OAuth2 token.
 getTarget :: Options -> String -> Text -> IO (Response Value)
 getTarget opts url token = do
     getWith (opts & auth ?~ oauth2Bearer token') url >>= asJSON 
     where
-        token' = utf8 # token
-
--- | Retrives the list of currently owned notes from Prosper.
---
--- Note: The query responses are paginated to 25 results per response.
-getNotesList :: Text -> IO [Text]
-getNotesList token = getNotesList' token 0
-
-getNotesList' :: Text -> Int -> IO [Text]
-getNotesList' token offset = do
-    resp <- getTarget opts (prosperURL "notes/") token
-    let ratings = resp ^.. _ratings
-    remainder <- if length ratings /= 25 then return [] else getNotesList' token (offset + 25)
-    return (ratings ++ remainder)
+        token' = utf8 # token        
+     
+getPaginated :: Text -> (V.Vector Value -> [a]) -> Text -> IO [a]
+getPaginated target parseFunc token = getPaginated' target parseFunc token 0
+        
+getPaginated' :: Text -> (V.Vector Value -> [a]) -> Text -> Int -> IO [a]
+getPaginated' target parseFunc token offset = do
+    resp <- getTarget opts (prosperURL target) token
+    let result = resp ^. responseBody . key "result" . _Array 
+    let vals = parseFunc result
+    remainder <- if length result /= 25 then return [] else getPaginated' target parseFunc token (offset + 25)
+    return (vals ++ remainder)
     where
         opts = defaults & param "offset" .~ [offset ^. re _Show . packed]
-        _ratings = responseBody . key "result" . _Array . traverse . key "prosper_rating" . _String
-
-getPendingIDs :: Text -> Int -> IO [Maybe Integer]
-getPendingIDs token offset = do
-    resp <- getTarget opts (prosperURL "orders/") token
-    let orders = resp ^. _orders
-    let bids = V.concat $ orders ^.. traverse . key "bid_requests" . _Array
-    let pendingBids = V.toList $ V.filter isPending bids
-    let pendingIDs = map getIDs pendingBids
-    remainder <- if length orders /= 25 then return [] else getPendingIDs token (offset + 25)
-    return (pendingIDs ++ remainder)
-    where
-        opts = defaults & param "offset" .~ [offset ^. re _Show . packed]
-        _orders = responseBody . key "result" . _Array 
+        
+parseRespForPendingIDs :: V.Vector Value -> [Integer]
+parseRespForPendingIDs result = catMaybes $ map listingID pendingBids
+    where        
+        bids = V.concat $ result ^.. traverse . key "bid_requests" . _Array
+        pendingBids = V.toList $ V.filter isPending bids
         isPending x = (x ^. key "bid_status" . _String) == "PENDING"
-        getIDs x = x ^? key "listing_id" . _Integer
+        listingID x = x ^? key "listing_id" . _Integer        
+        
+parseRespForNotesRating :: V.Vector Value -> [Text]
+parseRespForNotesRating resp = resp ^.. traverse 
+                                      . key "prosper_rating" 
+                                      . _String
 
-getRatingOf :: Text -> [Integer] -> IO [Text]
-getRatingOf token listingIDs = do   
+getNoteRatings :: Text -> IO [Text]
+getNoteRatings = getPaginated "notes/" parseRespForNotesRating
+
+getPendingNoteRatings :: Text -> IO [Text]
+getPendingNoteRatings token = do
+    pendingIDs <- getPaginated "orders/" parseRespForPendingIDs token
+    getRatingsOf token pendingIDs
+                         
+getRatingsOf :: Text -> [Integer] -> IO [Text]
+getRatingsOf token listingIDs = do   
     resp <- getTarget defaults (prosperURL searchQuery) token
     return $ resp ^.. _ratings
     where
         searchQuery = ("search/listings/?listing_number=" ++ toString listingIDs) ^. packed
         toString xs = intercalate  "," $ map show xs
         _ratings = responseBody . key "result" . _Array . traverse . key "prosper_rating" . _String
-
-reduceMaybe :: [Maybe a] -> [a]
-reduceMaybe = foldl (\acc x -> extractMaybe x ++ acc) []
-    where
-        extractMaybe Nothing = []
-        extractMaybe (Just n) = [n]
         
 main :: IO ()
 main = do
     (clientID:clientSecret:userID:password:xs) <- getArgs 
     token <- oauthToken clientID clientSecret userID password
-    pendingIDs <- getPendingIDs token 0
-    pendingNotes <- getRatingOf token $ reduceMaybe pendingIDs 
-    notes <- getNotesList token
+    notes <- getNoteRatings token
+    pendingNotes <- getPendingNoteRatings token
     print $ recommendNotes (notes++pendingNotes) [0, 0, 0.20, 0.20, 0.30, 0.25, 0.05] 3
-    
+        
